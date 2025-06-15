bff3e9ef335076e5d8be8fbfd674ac62f20f8b32
rizwan-106 <ahmadrizz15@gmail.com>
2025-06-15 09:46:56 +0530

    database updated

--- src/main/java/com/tcs/ChatAppBackendApplication.java
+++ src/main/java/com/tcs/ChatAppBackendApplication.java
@@ -9,8 +9,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ChatAppBackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-        System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+//		Dotenv dotenv = Dotenv.load();
+//        System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
 		SpringApplication.run(ChatAppBackendApplication.class, args);
 	}
 
