aabf613bf41b993a104af767a7f0bc70b6ad48ad
rizwan-106 <ahmadrizz15@gmail.com>
2025-06-16 19:29:09 +0530

    cors issue

--- src/main/java/com/tcs/ChatAppBackendApplication.java
+++ src/main/java/com/tcs/ChatAppBackendApplication.java
@@ -11,8 +11,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ChatAppBackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-        System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+//		Dotenv dotenv = Dotenv.load();
+//        System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(ChatAppBackendApplication.class, args);
 	}
